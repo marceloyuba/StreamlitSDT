f8384ba9aafb9edf7b6f91cde4c159eb04c945d2
micjb <micjb.94@gmail.com>
2024-05-09 13:34:54 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ local_css("style/style.css")
 
 
 st.text(" " * 250)    
-st.image("scr/SDTLogoC.png",width=1200, use_column_width=False, output_format='auto')
+st.image("scr/SDTLogoC.png",width=1200, use_column_width=True, output_format='auto')
 st.title("")
     
 
