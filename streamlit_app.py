3b88f6c50bfa8594b1e24107348f165646b83e58
micjb <micjb.94@gmail.com>
2024-05-10 17:24:48 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
 local_css("style/style.css")
-
+st.title("")
 
 column_widths = [1, 3, 1]
 with st.container():
@@ -26,8 +26,21 @@ with st.container():
         st.text("")
         
         
-st.title("")
-    
+st.header("Sobre nosotros")
+
+st.subheader("""
+Strategic Data Transform es una consultora de información, para generar estrategias de negocios, basado en datos.
+Se basa en un tipo de negocio B2B. Como empresa, ofrecer servicios a otras empresas, que estén dentro de la esfera del IT, tanto como empresas no relacionadas a la actividad y tengan necesidades de analizar sus datos y que permita poder desarrollar mejor sus actividades, teniendo conciencia de donde se encuentran las fallas en relación con cómo administran su información de negocio. 
+""")
+
+st.header("Nuestra propuesta de valor")
+st.subheader("""
+Dashboards de alto impacto.
+Nuestra idea es hacer dashboards de un gran impacto visual como a la vez completamente funcionales, interactivos e intuitivos para el usuario. En la actualidad se muestran muchas métricas y gráficos de manera apilada, sin dejar espacios libres, haciéndolos densos para ser leídos e interpretados. Nuestra idea es usar gráficos que sean más originales, respetando su imagen corporativa, pero un poco más desafiantes que paneles convencionales, que simplemente sean funcionales.
+
+Analisis Cientificos de datos.
+Gracias a las nuevas tecnologias y librerias disponibles para las mismas, podemos realizar analisis de datos basandonos en complementos del lenguaje de programacion Python, tales como Pandas, Seaborn y Mathplotlib y por supuesto, Machine Learning.
+""")
 
   
 with st.container():
