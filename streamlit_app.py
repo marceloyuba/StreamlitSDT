93b0ff8cbc9e7be49affcbee47e3f7d481503d56
micjb <micjb.94@gmail.com>
2024-05-10 17:38:26 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
 local_css("style/style.css")
-st.title("")
+
 
 column_widths = [1, 3, 1]
 with st.container():
