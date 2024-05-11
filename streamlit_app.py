a3f3c3c0ef7f168b6b3ad62e8bef08a7bb7920c2
micjb <micjb.94@gmail.com>
2024-05-11 18:37:00 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,6 +124,7 @@ st.write("""
 <style>
     .botones a {
         font-size: 30px;
+         font-family: Trebuchet MS, sans-serif;
         margin-top: 10px; /* Espacio después de la imagen */
         text-decoration: none; /* Quitar el subrayado */
         color: #ffffff; /* Cambiar el color del texto del enlace */
