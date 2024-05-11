9158c49272e7fa3b355a98a903e5931bc2de59bc
micjb <micjb.94@gmail.com>
2024-05-11 19:16:56 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write("""
         position: relative;
         top: 0px;
         padding: 10px;
-        left: 0%;
+        left: 10%;
         background-image:img src="https://github.com/marceloyuba/StreamlitSDT/blob/main/scr/SDTLogoC.png?raw=true" ;
         background-color: rgba(0, 0, 0, 0);
         color: #ffffff;
