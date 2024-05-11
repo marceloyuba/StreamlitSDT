11aa4771f17a329fb3d6fa5257511c3f9326bb54
micjb <micjb.94@gmail.com>
2024-05-11 15:47:20 -0300

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,6 @@ def local_css(file_name):
         
 local_css("style/style.css")
 
-
-st.sidebar.markdown("Seleccione una opcion")
-slider_label = "[Texto con hipervínculo](https://www.ejemplo.com)"
-st.sidebar.markdown(slider_label, unsafe_allow_html=True)
-
-
 column_widths = [1, 3, 1]
 with st.container():
     st.title("")
